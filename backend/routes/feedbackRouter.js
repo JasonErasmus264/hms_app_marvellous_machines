566f94bc5b990333ff8c5f0e91318a2f299808dc
Dale8246 <143092467+Dale8246@users.noreply.github.com>
2024-09-19 16:06:10 +0200

    feedback router edit

--- backend/routes/feedbackRouter.js
+++ backend/routes/feedbackRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import verifyToken from '../middleware/verifyToken.js';
+import { downloadMarksXLSX, downloadMarksCSV } from '../controllers/feedbackController.js';
 
 const feedbackRoute = express.Router();
 
